b67a0445944582af9b16a23c3170b8b07eb2518b
BestSherif <MurzichStar@gmail.com>
2022-10-12 14:30:22 +0300

    Count number of words

--- viseversa/views.py
+++ viseversa/views.py
@@ -4,5 +4,7 @@ def home(request):
     return render(request,"home.html")
 def reverse(request):
     user_text = request.GET['usertext']
+    count_string = user_text.split()
+    number_of_words = len(count_string)
     reversed_text = user_text[::-1]
-    return render(request,'reverse.html', {'usertext': user_text, 'reversedtext':reversed_text})
+    return render(request,'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text, 'numberofwords': number_of_words})
